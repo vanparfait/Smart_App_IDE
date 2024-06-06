636af5237167d1e2b5e877442ef994076897ece8
vanparfait <nguemechiaparfait@gmail.com>
2024-06-06 09:44:59 +0100

    Ajout de la fonctionnalite de changement de code

--- src/components/CodeTab.jsx
+++ src/components/CodeTab.jsx
@@ -1,14 +1,16 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { updateCode } from "../features/Tabs";
 
 const CodeTab = ({ code, id }) => {
   const tabsState = useSelector((state) => state.tabs);
+  const dispatch = useDispatch();
 
   console.log(tabsState);
 
   return (
     <textarea
+      onChange={(e) => dispatch(updateCode({ id, value: e.target.value }))}
       value={code}
       spellCheck="false"
       className="bg-zinc-900 text-slate-200 text-xl p-8 block h-full w-full focus:outline-none resize-none"
